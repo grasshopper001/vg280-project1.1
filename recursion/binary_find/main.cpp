2b93f18e9a15aadfb09c7b9eed0e901891fcd4e6
sunyi <grasshopper001@sjtu.edu.cn>
2017-01-29 02:38:38 +0800

    binary_find

--- recursion/binary_find/main.cpp
+++ recursion/binary_find/main.cpp
@@ -5,29 +5,25 @@
 #include <iostream>
 #include <cmath>
 const static int array[15]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14};
-enum bound{upperbound,lowerbound,target};
 
-int binary_bound(int* bound){
-    int mid=(int)floor((bound[upperbound]+bound[lowerbound])/2);
-    if(array[mid]==bound[target]){
+int binary_bound(int upperbound,int lowerbound,int target){
+    int mid=(upperbound+lowerbound)/2;
+    if(array[mid]==target){
         return mid;
     }
-    if(bound[upperbound]<=bound[lowerbound]){
+    if(upperbound<=lowerbound){
         return -1;
     }
-    if(array[mid]>bound[target]){
-        bound[upperbound]=mid-1;
-        return binary_bound(bound);
+    if(array[mid]>target){
+        return binary_bound(mid-1,lowerbound,target);
     }
-    if(array[mid]<bound[target]){
-        bound[lowerbound]=mid+1;
-        return binary_bound(bound);
+    if(array[mid]<target){
+        return binary_bound(mid+1,lowerbound,target);
     }
     return -1;
 }
 
 int main(){
-    int bound[3]={0,14,7};
-    std::cout<<binary_bound(bound)<<std::endl;
+    std::cout<<binary_bound(0,14,7)<<std::endl;
     return 0;
 }
