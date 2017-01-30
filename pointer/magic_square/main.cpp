afeaf10fd1a6702d04f431aad4804d48c24b1cd1
sunyi <grasshopper001@sjtu.edu.cn>
2017-01-30 17:20:39 +0800

    magic_square_mod2

--- pointer/magic_square/main.cpp
+++ pointer/magic_square/main.cpp
@@ -3,20 +3,50 @@
 //
 #include <iostream>
 #include <cmath>
+#include <iomanip>
+#define swap(a,b) a=a^b;b=a^b;a=a^b;
+
 class magic_square{
 private:
     int**digit;
     int n;
-    bool init(int currentNumber,int row,int col){
+    bool init_odd(int currentNumber,int row,int col){
         if(currentNumber>pow(n,2)){
             return true;
         }
         if(row>=0&&col>=0&&row<n&&col<n){
-            if(digit[row][col]==0){digit[row][col]=currentNumber;return init(currentNumber+1,row-1,col+1);}
-            else{return init(currentNumber,row+1,col);}
+            if(digit[row][col]==0){digit[row][col]=currentNumber;return init_odd(currentNumber+1,row-1,col+1);}
         }
-        if(row<0){return init(currentNumber,n-1,col);}
-        if(col>=n){return init(currentNumber,row,0);}
+        if(row<0&&col<n){return init_odd(currentNumber,n-1,col);}
+        if(col>=n&&row>=0){return init_odd(currentNumber,row,0);}
+        return init_odd(currentNumber,row+2,col-1);
+    }
+    void init_mod2(){
+        magic_square sample(n/2);
+        for(int i=0;i<n/2;i++){
+            for(int j=0;j<n/2;j++){
+                this->digit[i][j]=sample.digit[i][j];
+                this->digit[i+n/2][j+n/2]=sample.digit[i][j]+n*n/4;
+                this->digit[i][j+n/2]=sample.digit[i][j]+n*n/2;
+                this->digit[i+n/2][j]=sample.digit[i][j]+n*n/4*3;
+            }
+        }
+        int k=(n-2)/4;
+        for(int i=0;i<n/2;i++){
+            if(i==n/4){
+                for(int j=0;j<k;j++){
+                    swap(this->digit[i][j+n/4],this->digit[i+n/2][j+n/4]);
+                }
+            }
+            else{
+                for(int j=0;j<k;j++){
+                    swap(this->digit[i][j],this->digit[i+n/2][j]);
+                }
+            }
+            for(int j=0;j<k-1;j++){
+                swap(this->digit[i][n-1-n/4-j],this->digit[i+n/2][n-1-n/4-j]);
+            }
+        }
     }
 public:
     magic_square(int n):n(n){
@@ -24,12 +54,13 @@ public:
         for(int i=0;i<n;i++){
             digit[i]=(int*)calloc(n,sizeof(int));
         }
-        init(1,0,n/2);
+        if(n!=n/2*2) {init_odd(1,0,n/2);}
+        else if(n!=n/4*4){init_mod2();}
     }
     void print(){
         for(int i=0;i<n;i++){
             for(int j=0;j<n;j++){
-                std::cout<<digit[i][j]<<" ";
+                std::cout<<std::setw(4)<<digit[i][j]<<" ";
             }
             std::cout<<std::endl;
         }
@@ -43,7 +74,7 @@ public:
 };
 
 int main(){
-    magic_square square(3);
+    magic_square square(10);
     square.print();
     return 0;
 }
